673fbd45b3d06c2de899a9f19d1d6b0984e4a8e4
Elijah Gasataya <gasatae1@tcnj.edu>
2023-12-15 12:56:26 -0500

    added section for Capstone research projects

--- js/scroll_transition_script.js
+++ js/scroll_transition_script.js
@@ -1,9 +1,7 @@
-
-
-function reveal(){
+function reveal() {
   // Target all HTML elements that have the .reveal CSS class
-  var reveals = document.querySelectorAll('.reveal');
-  
+  var reveals = document.querySelectorAll('.reveal')
+
   console.log('Hello world!!')
   console.log('Number of elements that have the reveal tag: ' + reveals.length)
 
@@ -14,12 +12,11 @@ function reveal(){
     //Retreive the distance of a reveal element relative to the top of the page
     var elementTop = reveals[i].getBoundingClientRect().top
     console.log(elementTop)
-    var elementVisible = 150
 
     /* Add the "active" CSS class to element when that
          elem was scrolled a certain number of pixels
         */
-    if (elementTop < windowHeight - elementVisible) {
+    if (elementTop < windowHeight) {
       reveals[i].classList.add('active')
     } else {
       reveals[i].classList.remove('active')
@@ -29,6 +26,5 @@ function reveal(){
 
 /* Initialize event listener to run reveal() function every time the visitor
 scrolls the page in ANY direction (b/c of if conditional)*/
-window.addEventListener("scroll", reveal);
-reveal();
-
+window.addEventListener('scroll', reveal)
+reveal()
